8ae7c40a463147accea76b677f579eeefd7801e8
Alexander Tkachov <alexander@tkachov.ru>
2024-04-20 13:39:51 +0200

    MT: wtf websockets

--- ModdingTool/MainWindow.xaml.cs
+++ ModdingTool/MainWindow.xaml.cs
@@ -13,7 +13,6 @@ using System.Collections.ObjectModel;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Net.WebSockets;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Windows;
